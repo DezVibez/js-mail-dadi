fa9f3060dd2d2c4c7b25171dd9dba1eb81ff30ef
Emanuele <103491766+DezVibez@users.noreply.github.com>
2022-06-24 15:11:42 +0200

    ESERCIZIO MAIL FINITO

---
+++ script.js
@@ -0,0 +1,29 @@
+console.log("js ok")
+
+/*Mail
+Crea (inventa) una lista di inidirizzi mail autorizzati.
+Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa in console un messaggio appropriato sull’esito del controllo. ("Benvenuto" se la mail è nella lista, "Spiacenti" se non lo è);*/
+
+const indirizzi = ["Gennaro.Mancuso@gmail.com", "Fabio.Miceli@gmail.com", "Marcello.Padoan@gmail.com", "Piero.Brambilla@gmail.com"]
+
+
+
+let mail = prompt("Inserisci qui la tua mail" ,"Gennaro.Mancuso@gmail.com")
+
+for (let i = 0; i < indirizzi.length; i++) {
+    
+    
+    if (mail == indirizzi[i] ){
+        
+        console.log("Benvenuto")
+        break
+    }
+    else{
+        console.log("Spiacenti")
+        break
+    }
+    
+}
+
